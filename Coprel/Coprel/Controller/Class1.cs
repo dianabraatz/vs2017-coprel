8f0b036a24e7d1a05b46b7b2a06029a8e34381c1
charlan matter <mattergremio2@gmail.com>
2018-09-07 16:02:33 -0300

    teste

---
+++ Coprel/Coprel/Controller/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coprel.Controller
+{
+    class Class1
+    {
+        //teste
+    }
+}
